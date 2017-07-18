9a21b4f1413d30ee7492ffdf8da8d41bd5f0128d
Ricardo Pereira <m@ricardopereira.eu>
2017-07-19 00:17:07 +0100

    Detect horizontal planes

--- MeasureThings/ViewController.swift
+++ MeasureThings/ViewController.swift
@@ -33,6 +33,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         distanceLabel.textColor = .red
         distanceLabel.frame = CGRect(x: 5, y: 5, width: 150, height: 25)
         view.addSubview(distanceLabel)
+
+        setupFocusSquare()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -68,24 +70,51 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             return
         }
 
-        // Create a transform with a translation of 0.1 meters (10 cm) in front of the camera
-        var translation = matrix_identity_float4x4
-        translation.columns.3.z = -0.1
-        // Add a node to the session
-        let sphere = SCNSphere(radius: 0.005)
-        sphere.firstMaterial?.diffuse.contents = UIColor.red
-        sphere.firstMaterial?.lightingModel = .constant
-        sphere.firstMaterial?.isDoubleSided = true
-        let sphereNode = SCNNode(geometry: sphere)
-        sphereNode.simdTransform = simd_mul(currentFrame.camera.transform, translation)
-        sceneView.scene.rootNode.addChildNode(sphereNode)
+        let planeHitTestResults = sceneView.hitTest(view.center, types: .existingPlaneUsingExtent)
+        if let result = planeHitTestResults.first {
+            let hitPosition = SCNVector3.positionFromTransform(result.worldTransform)
+            let sphere = SCNSphere(radius: 0.005)
+            sphere.firstMaterial?.diffuse.contents = UIColor.red
+            sphere.firstMaterial?.lightingModel = .constant
+            sphere.firstMaterial?.isDoubleSided = true
+            let node = SCNNode(geometry: sphere)
+            node.position = hitPosition
+            sceneView.scene.rootNode.addChildNode(node)
 
-        if let startNode = startNode {
-            endNode = sphereNode
-            self.distanceLabel.text = String(format: "%.2f", distance(startNode: startNode, endNode: sphereNode)) + "m"
+            if let startNode = startNode {
+                endNode = node
+                let vector = startNode.position - node.position
+                let formatter = NumberFormatter()
+                formatter.numberStyle = .decimal
+                formatter.roundingMode = .ceiling
+                formatter.maximumFractionDigits = 2
+                // Scene units map to meters in ARKit.
+                distanceLabel.text = "Distance: " + formatter.string(from: NSNumber(value: vector.length()))! + " m"
+            }
+            else {
+                startNode = node
+            }
         }
         else {
-            startNode = sphereNode
+            // Create a transform with a translation of 0.1 meters (10 cm) in front of the camera
+            var translation = matrix_identity_float4x4
+            translation.columns.3.z = -0.1
+            // Add a node to the session
+            let sphere = SCNSphere(radius: 0.005)
+            sphere.firstMaterial?.diffuse.contents = UIColor.red
+            sphere.firstMaterial?.lightingModel = .constant
+            sphere.firstMaterial?.isDoubleSided = true
+            let sphereNode = SCNNode(geometry: sphere)
+            sphereNode.simdTransform = simd_mul(currentFrame.camera.transform, translation)
+            sceneView.scene.rootNode.addChildNode(sphereNode)
+
+            if let startNode = startNode {
+                endNode = sphereNode
+                self.distanceLabel.text = String(format: "%.2f", distance(startNode: startNode, endNode: sphereNode)) + "m"
+            }
+            else {
+                startNode = sphereNode
+            }
         }
     }
 
@@ -95,4 +124,107 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         return sqrtf(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z)
     }
 
+    var dragOnInfinitePlanesEnabled = false
+
+    // MARK: - ARSCNViewDelegate
+
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        DispatchQueue.main.async {
+            self.updateFocusSquare()
+        }
+    }
+
+    // MARK: - Focus Square
+
+    var focusSquare = FocusSquare()
+
+    func setupFocusSquare() {
+        focusSquare.unhide()
+        focusSquare.removeFromParentNode()
+        sceneView.scene.rootNode.addChildNode(focusSquare)
+    }
+
+    func updateFocusSquare() {
+        let (worldPosition, planeAnchor, _) = worldPositionFromScreenPosition(view.center, objectPos: focusSquare.position)
+        if let worldPosition = worldPosition {
+            focusSquare.update(for: worldPosition, planeAnchor: planeAnchor, camera: sceneView.session.currentFrame?.camera)
+        }
+    }
+
 }
+
+extension ViewController {
+
+    // Code from Apple PlacingObjects demo: https://developer.apple.com/sample-code/wwdc/2017/PlacingObjects.zip
+
+    func worldPositionFromScreenPosition(_ position: CGPoint,
+                                         objectPos: SCNVector3?,
+                                         infinitePlane: Bool = false) -> (position: SCNVector3?, planeAnchor: ARPlaneAnchor?, hitAPlane: Bool) {
+
+        // -------------------------------------------------------------------------------
+        // 1. Always do a hit test against exisiting plane anchors first.
+        //    (If any such anchors exist & only within their extents.)
+
+        let planeHitTestResults = sceneView.hitTest(position, types: .existingPlaneUsingExtent)
+        if let result = planeHitTestResults.first {
+
+            let planeHitTestPosition = SCNVector3.positionFromTransform(result.worldTransform)
+            let planeAnchor = result.anchor
+
+            // Return immediately - this is the best possible outcome.
+            return (planeHitTestPosition, planeAnchor as? ARPlaneAnchor, true)
+        }
+
+        // -------------------------------------------------------------------------------
+        // 2. Collect more information about the environment by hit testing against
+        //    the feature point cloud, but do not return the result yet.
+
+        var featureHitTestPosition: SCNVector3?
+        var highQualityFeatureHitTestResult = false
+
+        let highQualityfeatureHitTestResults = sceneView.hitTestWithFeatures(position, coneOpeningAngleInDegrees: 18, minDistance: 0.2, maxDistance: 2.0)
+
+        if !highQualityfeatureHitTestResults.isEmpty {
+            let result = highQualityfeatureHitTestResults[0]
+            featureHitTestPosition = result.position
+            highQualityFeatureHitTestResult = true
+        }
+
+        // -------------------------------------------------------------------------------
+        // 3. If desired or necessary (no good feature hit test result): Hit test
+        //    against an infinite, horizontal plane (ignoring the real world).
+
+        if (infinitePlane && dragOnInfinitePlanesEnabled) || !highQualityFeatureHitTestResult {
+
+            let pointOnPlane = objectPos ?? SCNVector3Zero
+
+            let pointOnInfinitePlane = sceneView.hitTestWithInfiniteHorizontalPlane(position, pointOnPlane)
+            if pointOnInfinitePlane != nil {
+                return (pointOnInfinitePlane, nil, true)
+            }
+        }
+
+        // -------------------------------------------------------------------------------
+        // 4. If available, return the result of the hit test against high quality
+        //    features if the hit tests against infinite planes were skipped or no
+        //    infinite plane was hit.
+
+        if highQualityFeatureHitTestResult {
+            return (featureHitTestPosition, nil, false)
+        }
+
+        // -------------------------------------------------------------------------------
+        // 5. As a last resort, perform a second, unfiltered hit test against features.
+        //    If there are no features in the scene, the result returned here will be nil.
+
+        let unfilteredFeatureHitTestResults = sceneView.hitTestWithFeatures(position)
+        if !unfilteredFeatureHitTestResults.isEmpty {
+            let result = unfilteredFeatureHitTestResults[0]
+            return (result.position, nil, false)
+        }
+
+        return (nil, nil, false)
+    }
+
+}
+
